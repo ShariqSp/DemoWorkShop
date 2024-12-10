5bbcab979e169f4b6fcd26f85dc87d21b1e8e65e
shariq <sp.shariq03@gmail.com>
2024-12-10 12:17:22 +0530

    headless

--- demoworkshop/src/test/java/demoworkshop/DemoTest.java
+++ demoworkshop/src/test/java/demoworkshop/DemoTest.java
@@ -10,9 +10,14 @@ public class DemoTest {
 	@Test
 	void testRegi()
 	{
-		
-        // Initialize the ChromeDriver without setting the path
-        WebDriver driver = new ChromeDriver();
+		 ChromeOptions options = new ChromeOptions();
+	        options.addArguments("--headless"); // Run in headless mode
+	        options.addArguments("--no-sandbox"); // Required for running as root
+	        options.addArguments("--disable-dev-shm-usage"); // Avoid memory issues
+	        options.addArguments("--disable-gpu"); // Disable GPU (not needed in headless)
+	        options.addArguments("--window-size=1920,1080"); // Optional: Define resolution
+
+	        WebDriver driver = new ChromeDriver(options);
 		  
 		  driver.get("https://demowebshop.tricentis.com/");
 		  
